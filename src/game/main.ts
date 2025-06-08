1f2ca3fb02d252f3356d2972de34baa95429f8c4
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-06-08 17:11:36 -0300

    Update title and upgrade Phaser to version 3.90.0

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,9 +1,9 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
+import { Boot } from './scenes/Boot';
 import { Preloader } from './scenes/Preloader';
+import { MainMenu } from './scenes/MainMenu';
+import { Game as MainGame } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
 
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
