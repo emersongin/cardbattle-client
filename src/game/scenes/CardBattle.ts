e450aa03da467c156938191eef53efe3e7480e06
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-06-11 22:01:57 -0300

    feat: create centered window method

--- src/game/scenes/CardBattle.ts
+++ src/game/scenes/CardBattle.ts
@@ -13,10 +13,8 @@ export class CardBattle extends Scene
     }
 
     create () {
-        const x = this.scale.width / 2;
-        const y = (this.scale.height / 2) - (this.scale.height / 8 / 2);
-        const width = this.scale.width / 2;
-        const height = this.scale.height / 8;
-        new TextWindow(this,  x, y, width, height, 'Welcome to the Card Battle! This is a simple text window example using Phaser 3 and RexUI plugin.');
+        TextWindow.createCenteredWindow(this, 'Welcome to the Card Battle!', () => {
+            console.log('Text box closed');
+        });
     }
 }
--- src/game/scenes/ui/TextWindow.ts
+++ src/game/scenes/ui/TextWindow.ts
@@ -1,7 +1,16 @@
 import { TextBox } from 'phaser3-rex-plugins/templates/ui/ui-components';
 
 export class TextWindow extends TextBox {
-    constructor(scene: Phaser.Scene, x: number, y: number, width: number, height: number, text: string) {
+    #tween: Phaser.Tweens.Tween | null = null;
+    private constructor(
+        scene: Phaser.Scene, 
+        x: number, 
+        y: number, 
+        width: number, 
+        height: number, 
+        text: string,
+        callback?: () => void
+    ) {
         super(scene, {
             x,
             y,
@@ -21,6 +30,57 @@ export class TextWindow extends TextBox {
             }
         });
         this.layout();
+        this.setScale(1, 0);
+        if (!scene.input.keyboard) {
+            throw new Error('Keyboard input is not available in this scene.');
+        }
+        scene.input.keyboard.on('keydown-SPACE', () => {
+            if (this.isBusy()) return;
+            if (callback && this.isOpen()) callback();
+            this.isClose() ? this.open() : this.close();
+        });
         scene.add.existing(this);
+    }
+
+    static createCenteredWindow(scene: Phaser.Scene, text: string, callback?: () => void) {
+        const x = scene.cameras.main.centerX;
+        const y = scene.cameras.main.centerY;
+        const width = (scene.cameras.main.width / 12) * 11;
+        const height = (scene.cameras.main.height / 12);
+        return new TextWindow(scene,  x, y, width, height, text, callback);
+    }
+
+    isOpen() {
+        return this.scaleY === 1;
+    }
+
+    isClose() {
+        return this.scaleY === 0;
+    }
+
+    isAvailable() {
+        return !this.isBusy();
+    }
+
+    isBusy() {
+        return this.#tween !== null && this.#tween.isPlaying();
+    }
+
+    open() {
+        this.#tween = this.scene.tweens.add({
+            targets: this,
+            scaleY: 1,
+            duration: 300,
+            ease: 'Back.easeOut'
+        });
+    }
+
+    close() {
+        this.#tween = this.scene.tweens.add({
+            targets: this,
+            scaleY: 0,
+            duration: 300,
+            ease: 'Back.easeOut'
+        });
     }
 }
