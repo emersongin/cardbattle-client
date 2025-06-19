880ae8a0d44b7c5e40465458d65d03f4da9abaee
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-06-19 17:45:28 -0300

    feat: add api

--- src/App.vue
+++ src/App.vue
@@ -11,68 +11,15 @@ const canMoveSprite = ref();
 const phaserRef = ref();
 const spritePosition = ref({ x: 0, y: 0 });
 
-const changeScene = () => {
-
-    const scene = toRaw(phaserRef.value.scene) as MainMenu;
-
-    if (scene)
-    {
-        //  Call the changeScene method defined in the `MainMenu`, `Game` and `GameOver` Scenes
-        scene.changeScene();
+const api = {
+    execute: () => {
+        console.log('API executed');
     }
-
-}
-
-const moveSprite = () => {
-
-    if (phaserRef.value !== undefined)
-    {
-
-        const scene = toRaw(phaserRef.value.scene) as MainMenu;
-
-        if (scene)
-        {
-            // Get the update logo position
-            (scene as MainMenu).moveLogo(({ x, y }) => {
-
-                spritePosition.value = { x, y };
-
-            });
-        }
-    }
-
-}
-
-const addSprite = () => {
-
-    const scene = toRaw(phaserRef.value.scene) as Phaser.Scene;
-
-    if (scene)
-    {
-        // Add a new sprite to the current scene at a random position
-        const x = Phaser.Math.Between(64, scene.scale.width - 64);
-        const y = Phaser.Math.Between(64, scene.scale.height - 64);
-    
-        // `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
-        const star = scene.add.sprite(x, y, 'star');
-
-        //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
-        //  You could, of course, do this from within the Phaser Scene code, but this is just an example
-        //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
-        scene.add.tween({
-            targets: star,
-            duration: 500 + Math.random() * 1000,
-            alpha: 0,
-            yoyo: true,
-            repeat: -1
-        });
-    }
-
-}
+};
 
 // Event emitted from the PhaserGame component
-const currentScene = (scene: MainMenu) => {
-    canMoveSprite.value = (scene.scene.key !== "MainMenu");
+const currentScene = (scene: VueScene) => {
+    scene.setApi(api);
 }
 
 </script>
