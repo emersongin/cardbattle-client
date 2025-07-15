bc1c0ef263d9243c7a6c8451011774f5aeda9502
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-07-14 22:21:19 -0300

    test: cardset close all

--- src/game/scenes/TestContext.ts
+++ src/game/scenes/TestContext.ts
@@ -1,13 +1,12 @@
 // import { Cardset } from '@/game/ui/Cardset/Cardset';
 import { CardData } from '@/game/ui/types/CardData';
 import { VueScene } from './VueScene';
-// import { Cardset } from '../ui/Cardset/Cardset';
-// import { ColorsPoints } from '../types/ColorsPoints';
-// import { CardColors, CardType } from '../ui/Card/Card';
-// import { CardPoints } from '../ui/Card/types/CardPoints';
-// import { ColorsPoints } from '../types';
+import { Cardset } from '../ui/Cardset/Cardset';
+import { ColorsPoints } from '../types/ColorsPoints';
+import { CARD_HEIGHT, CARD_WIDTH, CardColors, CardType } from '../ui/Card/Card';
+import { CardPoints } from '../ui/Card/types/CardPoints';
 import BoardWindow from '../ui/BoardWindow/BoardWindow';
-// import { CommandWindow } from '../ui/CommandWindow';
+import { CommandWindow } from '../ui/CommandWindow';
 
 export class TestContext extends VueScene
 {
@@ -20,79 +19,86 @@ export class TestContext extends VueScene
     }
 
     create () {
-        const startPoints = {
-            cardPoints: {
-                ap: 0,
-                hp: 0
-            },
-            colorsPoints: {
-                red: 0,
-                blue: 0,
-                green: 0,
-                black: 0,
-                white: 0,
-                orange: 0
-            },
-            numberOfCardsInHand: 0,
-            numberOfCardsInDeck: 0,
-            numberOfWins: 0
-        };
-        const boardWindow = BoardWindow.createCentered(this, startPoints);
-        boardWindow.open();
-        const updatePoints = {
-            cardPoints: {
-                ap: 999,
-                hp: 999
-            },
-            colorsPoints: {
-                red: 99,
-                blue: 99,
-                green: 99,
-                black: 99,
-                white: 99,
-                orange: 0
-            },
-            numberOfCardsInHand: 99,
-            numberOfCardsInDeck: 99,
-            numberOfWins: 9
-        };
-        boardWindow.updating(updatePoints);
-        // const cards = [
-        //     {
-        //         UUID: '123e4567-e89b-12d3-a456-426614174000',
-        //         number: 1,
-        //         name: 'Test Card',
-        //         description: 'This is a test card description.',
-        //         color: 'blue' as CardColors,
-        //         imageName: 'card-picture',
-        //         hp: 10,
-        //         ap: 5,
-        //         typeId: 'battle' as CardType,
-        //         powerId: 'none',
-        //         cost: 1
-        //     },
-        //     {
-        //         UUID: '123e4567-e89b-12d3-a456-426614174444',
-        //         number: 1,
-        //         name: 'Test Power Card',
-        //         description: 'This is a test power card description.',
-        //         color: 'red' as CardColors,
-        //         imageName: 'card-picture',
-        //         hp: 0,
+        // const startPoints = {
+        //     cardPoints: {
         //         ap: 0,
-        //         typeId: 'power' as CardType,
-        //         powerId: 'power-1',
-        //         cost: 1
+        //         hp: 0
         //     },
-        // ];
-        // const cardsData: CardData[] = this.duplicate(cards, 3); // 40
-        // const dimensions = { 
-        //     x: this.cameras.main.centerX / 2, 
-        //     y: this.cameras.main.centerY - 75, 
-        //     width: (CARD_WIDTH * 6), 
-        //     height: CARD_HEIGHT 
+        //     colorsPoints: {
+        //         red: 0,
+        //         blue: 0,
+        //         green: 0,
+        //         black: 0,
+        //         white: 0,
+        //         orange: 0
+        //     },
+        //     numberOfCardsInHand: 0,
+        //     numberOfCardsInDeck: 0,
+        //     numberOfWins: 0
         // };
-        // const cardset = new Cardset(this, dimensions, cardsData);
+        // const boardWindow = BoardWindow.createBottom(this, startPoints);
+        // const boardWindow2 = BoardWindow.createTopReverse(this, startPoints);
+        // boardWindow.open();
+        // boardWindow2.open();
+        // const updatePoints = {
+        //     cardPoints: {
+        //         ap: 999,
+        //         hp: 999
+        //     },
+        //     colorsPoints: {
+        //         red: 99,
+        //         blue: 99,
+        //         green: 99,
+        //         black: 99,
+        //         white: 99,
+        //         orange: 0
+        //     },
+        //     numberOfCardsInHand: 99,
+        //     numberOfCardsInDeck: 99,
+        //     numberOfWins: 9
+        // };
+        // boardWindow.updating(updatePoints);
+        // boardWindow2.updating(updatePoints);
+        // boardWindow.close();
+        // boardWindow2.close();
+
+        const cards = [
+            {
+                UUID: '123e4567-e89b-12d3-a456-426614174000',
+                number: 1,
+                name: 'Test Card',
+                description: 'This is a test card description.',
+                color: 'blue' as CardColors,
+                imageName: 'card-picture',
+                hp: 10,
+                ap: 5,
+                typeId: 'battle' as CardType,
+                powerId: 'none',
+                cost: 1
+            },
+            {
+                UUID: '123e4567-e89b-12d3-a456-426614174444',
+                number: 1,
+                name: 'Test Power Card',
+                description: 'This is a test power card description.',
+                color: 'red' as CardColors,
+                imageName: 'card-picture',
+                hp: 0,
+                ap: 0,
+                typeId: 'power' as CardType,
+                powerId: 'power-1',
+                cost: 1
+            },
+        ];
+        const cardsData: CardData[] = this.duplicate(cards, 3); // 40
+        const dimensions = { 
+            x: this.cameras.main.centerX / 2, 
+            y: this.cameras.main.centerY - 75, 
+            width: (CARD_WIDTH * 6), 
+            height: CARD_HEIGHT 
+        };
+        const cardset = new Cardset(this, dimensions, cardsData);
+        cardset.closeAllCardsDomino();
         // const events = {
         //     onChangeIndex: (cardIndex: number) => {
         //         if (!cardset.isValidIndex(cardIndex)) return;
