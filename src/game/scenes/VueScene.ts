a62abd9c1d69f1c921ad4c97407a1cd7630b2f10
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-07-16 08:49:34 -0300

    feat: timeline in vue scene

--- src/game/scenes/VueScene.ts
+++ src/game/scenes/VueScene.ts
@@ -2,6 +2,19 @@ import { Scene } from 'phaser';
 import { EventBus } from '@game/EventBus';
 import { CardBattle } from '../api/CardBattle';
 
+export type TimelineConfig = {
+    targets: Phaser.GameObjects.Components.Transform[];
+    delay?: number;
+    eachDelay?: number;
+    durantion?: number;
+    eachDuration?: number;
+    x?: number;
+    eachX?: number;
+    y?: number;
+    eachY?: number;
+    onComplete?: () => void;
+}
+
 export class VueScene extends Scene {
     #cardBattle: CardBattle;
 
@@ -22,5 +35,40 @@ export class VueScene extends Scene {
             throw new Error('API not set in scene');
         }
         return this.#cardBattle;
+    }
+
+    timeline(
+        timiline: TimelineConfig
+    ): void {
+        const promises = timiline.targets.map((target: Phaser.GameObjects.Components.Transform, index: number) => {
+            return new Promise<void>((resolve) => {
+                let delay = 0;
+                let duration = 0;
+                let x = (target.x || 0);
+                let y = (target.y || 0);
+                if (timiline.delay !== undefined) delay = timiline.delay;
+                if (timiline.durantion !== undefined) duration = timiline.durantion;
+                if (timiline.x !== undefined) x = timiline.x;
+                if (timiline.x !== undefined) x = timiline.x;
+                if (timiline.eachX !== undefined) x += (index * timiline.eachX);
+                if (timiline.eachY !== undefined) y += (index * timiline.eachY);
+                if (timiline.eachDelay !== undefined) delay += (index * timiline.eachDelay);
+                if (timiline.eachDuration !== undefined) duration += ((index + 1) * timiline.eachDuration);
+                const tweenConfig = {
+                    targets: target,
+                    x, y, 
+                    delay, 
+                    duration, 
+                    hold: 0,
+                    onComplete: () => {
+                        resolve();
+                    },
+                };
+                this.tweens.add(tweenConfig);
+            });
+        });
+        Promise.all(promises).then(() => {
+            timiline.onComplete?.();
+        });
     }
 }
