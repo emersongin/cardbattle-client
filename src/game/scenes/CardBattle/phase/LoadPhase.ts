59b060488012997918d36e2ceb8c0d7ab0c9a536
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-07-28 17:47:37 -0300

    refactor: streamline LoadPhase management and enhance command window functionality

--- src/game/scenes/CardBattle/phase/LoadPhase.ts
+++ src/game/scenes/CardBattle/phase/LoadPhase.ts
@@ -1,111 +1,83 @@
 import { Phase } from "./Phase";
-import { CardBattleScene } from '../CardBattleScene';
-import { TextWindow } from '@game/ui/TextWindow';
+import { CardBattleScene } from "../CardBattleScene";
+import { CardBattlePhase } from "./CardBattlePhase";
 import { SummonPhase } from "./SummonPhase";
-import { CommandWindow } from "@game/ui/CommandWindow";
 import { TriggerPhase } from "./TriggerPhase";
 import { PowerSlots } from "../abs/PowerSlots";
+import { LOAD_PHASE } from "@/game/constants/Keys";
 
-export class LoadPhase extends PowerSlots implements Phase {
-    #textWindow: TextWindow;
-    #noTextWindow: boolean = false;
-    #commandWindow: CommandWindow;
-    #zoneCommandWindow: CommandWindow;
+export class LoadPhase extends CardBattlePhase implements Phase {
+    #powerSlots: PowerSlots;
+    #isStartPhase: boolean;
     
-    constructor(readonly scene: CardBattleScene, powerSlots: any[] = [], noTextWindow: boolean = false) {
-        super(powerSlots);
-        this.#noTextWindow = noTextWindow;
+    constructor(readonly scene: CardBattleScene, powerSlots: any[] = [], startPhase: boolean = true) {
+        super(scene);
+        this.#powerSlots = new PowerSlots(powerSlots);
+        this.#isStartPhase = startPhase;
     }
 
-    changeToChallengePhase(): void {
-        throw new Error("Method not implemented.");
-    }
-    
-    changeToStartPhase(): void {
-        throw new Error("Method not implemented.");
-    }
-
-    changeToDrawPhase(): void {
-        throw new Error("Method not implemented.");
-    }
-
-    changeToLoadPhase(): void {
-        this.scene.changePhase(new LoadPhase(this.scene, this.getPowerSlots()));
-    }
-
-    changeToTriggerPhase(origin: string): void {
-        this.scene.changePhase(new TriggerPhase(this.scene, this.getPowerSlots(), origin));
-    }
-
-    changeToSummonPhase(): void {
-        this.scene.changePhase(new SummonPhase(this.scene));
-    }
-
-    changeToCompilePhase(): void {
-        throw new Error("Method not implemented.");
-    }
-
-    changeToBattlePhase(): void {
-        throw new Error("Method not implemented.");
-    }
-
-    create(): void {
-        this.createTextWindow('Load Phase started!');
-        this.createCommandWindow('Select power card?');
-        this.createZoneCommandWindow('Select your zone');
-        if (this.#noTextWindow) {
-            this.openCommandWindow();
+    async create(): Promise<void> {
+        await super.createPlayerBoard();
+        await super.createOpponentBoard();
+        this.#createCommandWindow();
+        if (this.#isStartPhase === false) {
+            super.openPlayerBoard(() =>  super.openCommandWindow());
+            super.openOpponentBoard();
             return;
         }
-        this.openTextWindow();
+        this.#createLoadPhaseWindow();
+        super.openAllWindows();
     }
 
-    private createTextWindow(title: string): void {
-        this.#textWindow = TextWindow.createCentered(this.scene, title, {
+    #createLoadPhaseWindow(): void {
+        super.createTextWindowCentered('Load Phase started!', {
+            textAlign: 'center',
             onClose: () => {
-                this.openCommandWindow();
+                super.openPlayerBoard(() =>  super.openCommandWindow());
+                super.openOpponentBoard();
             }
         });
+        super.addTextWindow('Select and use a Power Card');
     }
 
-    private createCommandWindow(title: string): void {
-        this.#commandWindow = CommandWindow.createBottom(this.scene, title, [
+    #createCommandWindow(): void {
+        const options = [
             {
                 description: 'Yes',
                 onSelect: () => {
-                    this.openZoneCommandWindow();
+                    this.#createZoneCommandWindow();
+                    super.openCommandWindow();
                 }
             },
             {
                 description: 'No',
                 onSelect: () => {
-                    if (this.hasPower()) {
-                        const origin = 'LOAD';
-                        this.changeToTriggerPhase(origin);
+                    if (this.#powerSlots.hasPower()) {
+                        this.changeToTriggerPhase(LOAD_PHASE);
                         return;
                     }
                     this.changeToSummonPhase();
                 }
-            },
-        ]);
+            }
+        ];
+        super.createCommandWindowBottom('Use a Power Card?', options);
     }
 
-    private createZoneCommandWindow(title: string): void {
-        this.#zoneCommandWindow = CommandWindow.createBottom(this.scene, title, [
+    #createZoneCommandWindow(): void {
+        const options = [
             {
                 description: 'Play Power card',
                 onSelect: () => {
-                    this.addPowerSlot({
+                    this.#powerSlots.addPowerSlot({
                         action: 'POWER_1',
                         params: {
                             cardId: 'card_1',
                             zone: 'player'
                         } 
                     });
-                    console.log(this.powerSlotsTotal());
-                    if (this.isLimitReached()) {
-                        const origin = 'LOAD';
-                        this.changeToTriggerPhase(origin);
+                    console.log(this.#powerSlots.powerSlotsTotal());
+                    if (this.#powerSlots.isLimitReached()) {
+                        this.changeToTriggerPhase(LOAD_PHASE);
                         return;
                     }
                     this.changeToLoadPhase();
@@ -128,27 +100,49 @@ export class LoadPhase extends PowerSlots implements Phase {
                 onSelect: () => {
                     this.changeToLoadPhase();
                 }
-            },
-        ]);
-    }
-
-    openTextWindow(): void {
-        this.#textWindow.open();
-    }
-
-    openCommandWindow(): void {
-        this.#commandWindow.open();
-    }
-
-    openZoneCommandWindow(): void {
-        this.#zoneCommandWindow.open();
+            }
+        ];
+        super.createCommandWindowBottom('Select your zone', options);
     }
 
     update(): void {
         console.log("Updating Load Phase...");
     }
+
+        changeToChallengePhase(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+    changeToStartPhase(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    changeToDrawPhase(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    changeToLoadPhase(): void {
+        const startPhase = false;
+        this.scene.changePhase(new LoadPhase(this.scene, this.#powerSlots.getPowerSlots(), startPhase));
+    }
+
+    changeToTriggerPhase(origin: string): void {
+        this.scene.changePhase(new TriggerPhase(this.scene, this.#powerSlots.getPowerSlots(), origin));
+    }
+
+    changeToSummonPhase(): void {
+        this.scene.changePhase(new SummonPhase(this.scene));
+    }
+
+    changeToCompilePhase(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    changeToBattlePhase(): void {
+        throw new Error("Method not implemented.");
+    }
     
     destroy(): void {
-        if (this.#textWindow) this.#textWindow.destroy();
+        super.destroyAllTextWindows();
     }
 }
