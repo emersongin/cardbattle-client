dde37220a3b161a0c97b8ddaabe38e9e5e4b81cf
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-09-06 11:50:14 -0300

    refactor: paths

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -27,13 +27,16 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('../src', import.meta.url)),
-            '@tests': fileURLToPath(new URL('../src/tests', import.meta.url)),
             '@game': fileURLToPath(new URL('../src/game', import.meta.url)),
             '@api': fileURLToPath(new URL('../src/game/api', import.meta.url)),
+            '@constants': fileURLToPath(new URL('../src/game/constants', import.meta.url)),
+            '@objects': fileURLToPath(new URL('../src/game/objects', import.meta.url)),
             '@scenes': fileURLToPath(new URL('../src/game/scenes', import.meta.url)),
+            '@types': fileURLToPath(new URL('../src/game/types', import.meta.url)),
             '@ui': fileURLToPath(new URL('../src/game/ui', import.meta.url)),
             '@utils': fileURLToPath(new URL('../src/game/utils', import.meta.url)),
             '@mocks': fileURLToPath(new URL('../src/tests/mocks', import.meta.url)),
+            '@tests': fileURLToPath(new URL('../src/tests', import.meta.url))
         }
     },
     logLevel: 'warning',
